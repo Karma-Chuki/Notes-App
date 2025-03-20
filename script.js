2205553da5899e2751cada625343267e1b1e18a5
Karma-Chuki <109060808+Karma-Chuki@users.noreply.github.com>
2025-03-20 19:37:52 +0530

    more fixing

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const addbtn = document.querySelector('#add');
 const main = document.querySelector('#main');
 
 addbtn.addEventListener('click', function(){
+    alert('Button clicked!');
     addNote();
 });
 
@@ -19,37 +20,13 @@ const saveNote = () => {
     localStorage.setItem('notes', JSON.stringify(data));
 };
 
-// const saveNote = (note) => {
-//     //select text area
-//     const notes = document.querySelectorAll('.note .text');
-
-//     //select title area
-//     const title = document.querySelectorAll('.note .title');
-
-//     const data =[];
-
-//     notes.forEach((note, index) => {
-//         data.push({title: title[index].value, text: note.value});
-//         });
-//     const titlesData = data.map((note) => note.title);
-//     console.log(titlesData);
-//     localStorage.setItem(
-//         'titles', JSON.stringify(titlesData)
-//     );
-
-//     const textData = data.map((item) => item.text);
-//     localStorage.setItem(
-//         "notes", JSON.stringify(textData)
-//     );
-// };
-
 addNote = (text = "", title = "") => {
     const note = document.createElement('div');
     note.classList.add('note');
     note.innerHTML = `
         <div class="notes">
-            <i class = "far fa-save" style="color:yellow">&#128465;</i>
-            <i class = "fas fa-trash-alt" style="color:red">&#xf0c7;</i>
+            <i class = "far fa-save" style="color:yellow"></i>
+            <i class = "fas fa-trash-alt" style="color:red"></i>
         </div>
         <div class="title-div">
             <textarea class ="title" placeholder="write the title">${title}</textarea>
@@ -74,27 +51,15 @@ addNote = (text = "", title = "") => {
     saveNote();
 };
 
-// function loadNotes() {
-//     const titlesData = localStorage.getItem('titles');
-//     const textData = localStorage.getItem('notes');
-
-//     for (let i = 0; i < titlesData.length; i++) {
-//         addNote(textData[i], titlesData[i]);
-//     }
-// }
 function loadNotes() {
-    const titlesData = JSON.parse(localStorage.getItem('titles')) || [];
-    const textData = JSON.parse(localStorage.getItem('notes')) || [];
+    const data = JSON.parse(localStorage.getItem('notes')) || [];
 
-    if (titlesData && textData) {
-        for (let i = 0; i < titlesData.length; i++) {
-            addNote(textData[i], titlesData[i]);
-        }
-    }
+    data.forEach(note => {
+        addNote(note.text, note.title);
+    });
 }
 
 loadNotes();
 
 
-	// &#xf2ed;// trash icon
-    	// far fa-save	&#xf0c7;
+// &#128465; &#xf0c7;
